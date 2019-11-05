f607a9556580eeec3d11114b583ba832853fd991
Mustofa Kamal <mustofa2008@gmail.com>
2019-11-05 17:54:48 +0600

    add new codes

--- app/src/main/java/com/kamal/twiliovideo/Note.java
+++ app/src/main/java/com/kamal/twiliovideo/Note.java
@@ -34,4 +34,126 @@ public class Note {
 
     https://www.twilio.com/docs/runtime/functions-assets-api/quickstart#please-read-before-starting
     */
+
+
+    /*
+    =============================================
+
+    public static final String ACCOUNT_SID = "ACf92291edf2e4e6b90e2b437452ed5eff";
+    public static final String AUTH_TOKEN = "3e235fad509dd22c78eebefb79aa9db8";
+
+    =============================================
+    FRIENDLY NAME
+    TwilioVideo
+
+    SID
+    SK638e9993ed26cea3fb4da9755b2101ed
+
+    KEY TYPE
+    Standard
+
+    SECRET
+    iR3kvfJmseBFOgA8mSIaaXEy34qAsqWU
+
+    let accountSid = process.env.TWILIO_ACCOUNT_SID;
+    let authToken = process.env.TWILIO_AUTH_TOKEN;
+
+    let client = require('twilio')(accountSid, authToken);
+
+    let apiKey = process.env.TWILIO_API_KEY;
+    let secret = process.env.TWILIO_API_SECRET;
+
+    let client = require('twilio')(apiKey, secret);
+    ==============================================
+    exports.handler = function(context, event, callback) {
+        let AccessToken = Twilio.jwt.AccessToken;
+        let VideoGrant = AccessToken.VideoGrant;
+        let client = context.getTwilioClient();
+        //console.log(client.accountSid);
+        console.log(context.ACCOUNT_SID);
+        console.log(context.API_KEY);
+        console.log(context.API_SECRET);
+
+        let token = new AccessToken(context.ACCOUNT_SID, context.API_KEY, context.API_SECRET);
+        token.identity = event.identity;
+
+        console.log(token);
+
+        let videoGrant = new VideoGrant({
+            room: 'DailyStandup'
+        });
+
+        token.addGrant(videoGrant);
+
+        callback(null, {token: token.toJwt});
+    };
+    ================================================
+    exports.handler = function(context, event, callback) {
+        const AccessToken = require('twilio').jwt.AccessToken;
+        const VideoGrant = AccessToken.VideoGrant;
+
+        // Used when generating any kind of Access Token
+        const twilioAccountSid = 'ACf92291edf2e4e6b90e2b437452ed5eff';
+        const twilioApiKey = 'SK638e9993ed26cea3fb4da9755b2101ed';
+        const twilioApiSecret = 'iR3kvfJmseBFOgA8mSIaaXEy34qAsqWU';
+
+        // Create an access token which we will sign and return to the client,
+        // containing the grant we just created
+        const token = new AccessToken(twilioAccountSid, twilioApiKey, twilioApiSecret);
+        token.identity = 'TwilioVideo';
+
+        // Create a Video grant which enables a client to use Video
+        // and limits access to the specified Room (DailyStandup)
+        const videoGrant = new VideoGrant({
+            room: 'DailyStandup'
+        });
+
+        // Add the grant to the token
+        token.addGrant(videoGrant);
+
+        // Serialize the token to a JWT string
+        console.log(token.toJwt());
+
+        console.log(context.ACCOUNT_SID);
+        console.log(context.API_KEY);
+        console.log(context.API_SECRET);
+
+        callback(null, {token: token.toJwt});
+    };
+    =================================================
+    exports.handler = function(context, event, callback) {
+        const AccessToken = require('twilio').jwt.AccessToken;
+        const VideoGrant = AccessToken.VideoGrant;
+
+        // Used when generating any kind of Access Token
+        const twilioAccountSid = 'ACf92291edf2e4e6b90e2b437452ed5eff';
+        const twilioApiKey = 'SK638e9993ed26cea3fb4da9755b2101ed';
+        const twilioApiSecret = 'iR3kvfJmseBFOgA8mSIaaXEy34qAsqWU';
+
+        // Create an access token which we will sign and return to the client,
+        // containing the grant we just created
+        const token = new AccessToken(twilioAccountSid, twilioApiKey, twilioApiSecret);
+        token.identity = 'TwilioVideo';
+
+        // Create a Video grant which enables a client to use Video
+        // and limits access to the specified Room (DailyStandup)
+        const videoGrant = new VideoGrant({
+            room: 'DailyStandup'
+        });
+
+        // Add the grant to the token
+        token.addGrant(videoGrant);
+
+        // Serialize the token to a JWT string
+        console.log(token.toJwt());
+
+        console.log(context.ACCOUNT_SID);
+        console.log(context.API_KEY);
+        console.log(context.API_SECRET);
+
+        //callback(null, {token: token.toJwt});
+        callback(null, { token: token.toJwt() });
+    };
+
+    */
 }
