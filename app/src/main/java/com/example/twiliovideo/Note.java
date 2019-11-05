64c8ee2215b7629c222f3bd25d58a213116a8da3
himustofa <mustofa2008@gmail.com>
2019-11-05 09:14:56 +0600

    add some codes

---
+++ app/src/main/java/com/example/twiliovideo/Note.java
@@ -0,0 +1,37 @@
+package com.example.twiliovideo;
+
+public class Note {
+
+    /*
+    User Identity & Access Tokens
+    //=========================================================
+    https://www.twilio.com/docs/video/tutorials/user-identity-access-tokens#access-token-server-sample-applications
+    Example: https://github.com/twilio/video-quickstart-js
+    https://github.com/TwilioDevEd/video-access-token-server-node
+
+
+
+    User Identity & Access Tokens
+    //=========================================================
+    https://www.youtube.com/watch?v=5lrdYBLEk60
+
+
+
+    Building Video Applications on Android By Marcos Placona
+    //=========================================================
+    https://www.twilio.com/blog/2016/03/building-video-applications-on-android.html
+    https://github.com/mplacona/TwilioVideo
+
+
+
+    Note:
+    //=========
+    https://www.youtube.com/watch?v=PKodXk6L5Eo
+    https://www.youtube.com/watch?v=lkQczBYLniA&list=PLQm9P1qjjvxvZwV3Whv0Ehz4KUNe5ugNa&index=2
+
+    Java Library
+    https://www.twilio.com/docs/libraries/java
+
+    https://www.twilio.com/docs/runtime/functions-assets-api/quickstart#please-read-before-starting
+    */
+}
